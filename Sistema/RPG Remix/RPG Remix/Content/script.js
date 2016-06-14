5337a423f8141b0dbcc4e3f9626906e68d846c1e
Luiz de Queiroz <luizdequeiroz@outlook.com.br>
2016-06-14 09:03:49 -0300

    loading GIF

--- Sistema/RPG Remix/RPG Remix/Content/script.js
+++ Sistema/RPG Remix/RPG Remix/Content/script.js
@@ -1,1 +1,19 @@
-﻿
+﻿function loaderOn() {
+    $('.loading').css('display', 'normal');
+}
+function loaderOff() {
+    $('.loading').css('display', 'none');
+}
+
+var id_old;
+
+function menuClick(id_clicked) {
+
+    $('#' + id_old).css('border', '');
+    $('#' + id_clicked).css('border', 'solid 3px darkred');
+
+    id_old = id_clicked;
+
+    $('#pagina').val(id_clicked);
+    $('#menu-form').submit();
+}
