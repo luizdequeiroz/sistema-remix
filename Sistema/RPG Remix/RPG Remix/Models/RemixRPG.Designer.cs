cd40083bcc0ca3b07af653a5b3f321d6d3c432c5
luizdequeiroz <luizdequeiroz@outlook.com.br>
2016-06-16 20:51:59 -0300

    Correções nos relacionamentos e adição das tabelas Região e Mapa.

--- Sistema/RPG Remix/RPG Remix/Models/RemixRPG.Designer.cs
+++ Sistema/RPG Remix/RPG Remix/Models/RemixRPG.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Repos\Remix\Sistema\RPG Remix\RPG Remix\Models\RemixRPG.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Repos\Sistema\RPG Remix\RPG Remix\Models\RemixRPG.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
